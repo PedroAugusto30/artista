1e4f50ea5ae25ad300bddf9bb072395052dc997d
PedroAugusto30 <169068215+PedroAugusto30@users.noreply.github.com>
2024-05-13 15:05:03 +0000

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,26 @@
+
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("      Veigh      ");
+    System.out.println(" ");
+    System.out.println("   Album:Dos Prédios Deluxe");
+    System.out.println(" ");
+    System.out.println("   Lançamento:20 de maio de 2023");
+    System.out.println(" ");
+    System.out.println("   Informação:O album deluxe é a continuação do album dos prédio");
+    System.out.println(" ");
+    System.out.println(" ");
+    System.out.println("      Músicas      ");
+    System.out.println(" ");
+    System.out.println("       Nome                  Duração");
+    System.out.println(" 1.  Novo Balanço               2:20");
+    System.out.println(" 2.  Engana Dizendo Que Ama     3:08");
+    System.out.println(" 3.  Bolsa De Ombro             1:54");
+    System.out.println(" 4.  Tendenciosa                3:15");
+    System.out.println(" 5.  Clickbait                  2:41");
+    System.out.println(" 6.  Turma do Bairro            1:52");
+    System.out.println(" 7.  Devolve as Correntes       2:20");
+    System.out.println(" 8.  Hora Errada                3:30");
+    System.out.println(" 9.  Detalhes                   3:33");
+  }
+}
